3e2cbf2948d7c37ce36aceda4aba72c738bd8f95
ManeeshNadcab <maneeshnadcab@gmail.com>
2024-04-02 15:46:28 +0530

    ohk done v-4

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const  TRUSTAPP_API="https://trst.ink/business/protected-route";
 const localip = "192.168.1.10";
 const serverip = "64.227.149.24";
-export const  AUTH_API_URL = `http://${localip}:2222/auth/api`;
-export const  WALLET_API_URL = `http://${localip}:2222/wallet/api`;
-export const  STAKE_API_URL = `http://${localip}:2222/stake/api`;
+export const  AUTH_API_URL = `http://${serverip}:2222/auth/api`;
+export const  WALLET_API_URL = `http://${serverip}:2222/wallet/api`;
+export const  STAKE_API_URL = `http://${serverip}:2222/stake/api`;
