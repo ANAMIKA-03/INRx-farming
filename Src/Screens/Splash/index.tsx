d4539a245bb2158ca7f22087bb005b2a2a77c095
ANAMIKA-03 <anamika1965p@gmail.com>
2024-06-05 11:09:14 +0530

    SplashAnimation

--- Src/Screens/Splash/index.tsx
+++ Src/Screens/Splash/index.tsx
@@ -1,22 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Image,
-  Text,
-  ImageBackground,
-  StatusBar,
-  SafeAreaView,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, ImageBackground, StatusBar, SafeAreaView } from 'react-native';
 import Styles from './Styles';
 import Images from '../../Styles/Images';
 import Animated, {
   useSharedValue,
   withDelay,
   withTiming,
+  useAnimatedStyle,
 } from 'react-native-reanimated';
-import {useDispatch, useSelector} from 'react-redux';
-import {RootState} from '../../Services/Redux/store';
-import {setAppInit} from '../../Services/Redux/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../Services/Redux/store';
+import { setAppInit } from '../../Services/Redux/authSlice';
 
 const DURATION = 1000;
 const DELAY = 500;
@@ -28,33 +22,24 @@ export type Props = {
 };
 
 const Splash = (props: any) => {
-  const {navigation} = props;
-  const {login, user, tokenId, initapplication} = useSelector(
-    (state: RootState) => state.auth,
-  );
+  const { navigation } = props;
+  const { login, user, tokenId, initapplication } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
   const [isShown, setShown] = useState(false);
 
-  const opacity1 = useSharedValue(0);
-  const opacity2 = useSharedValue(0);
-  const opacity3 = useSharedValue(0);
-  const opacity4 = useSharedValue(0);
+  const translations = text.map(() => useSharedValue(0));
 
-  // prettier-ignore
   const show = () => {
-    if (isShown) {
-      opacity4.value = withDelay(0 * DELAY, withTiming(0, { duration: DURATION }));
-      opacity3.value = withDelay(1 * DELAY, withTiming(0, { duration: DURATION }));
-      opacity2.value = withDelay(2 * DELAY, withTiming(0, { duration: DURATION }));
-      opacity1.value = withDelay(3 * DELAY, withTiming(0, { duration: DURATION }));
-    } else {
-      opacity1.value = withDelay(0 * DELAY, withTiming(1, { duration: DURATION }));
-      opacity2.value = withDelay(1 * DELAY, withTiming(1, { duration: DURATION }));
-      opacity3.value = withDelay(2 * DELAY, withTiming(1, { duration: DURATION }));
-      opacity4.value = withDelay(3 * DELAY, withTiming(0, { duration: DURATION }));
-    }
-
     setShown(!isShown);
+    if (!isShown) {
+      text.forEach((_, index) => {
+        translations[index].value = withDelay(index * DELAY, withTiming(1, { duration: DURATION }));
+      });
+    } else {
+      text.forEach((_, index) => {
+        translations[index].value = withDelay(index * DELAY, withTiming(0, { duration: DURATION }));
+      });
+    }
   };
 
   const setAppInitState = () => dispatch(setAppInit(true));
@@ -76,7 +61,6 @@ const Splash = (props: any) => {
   useEffect(() => {
     show();
     const timer = setTimeout(() => {
-      // navigation.navigate('Onboarding');
       navigateToScreen();
     }, 4000);
     return () => clearTimeout(timer);
@@ -89,18 +73,20 @@ const Splash = (props: any) => {
         <Image source={Images.logo} style={Styles.logoIcon} />
 
         <View style={Styles.text}>
-          <Animated.Text style={{...Styles.logoText, opacity: opacity1}}>
-            {text[0]}
-          </Animated.Text>
-          <Animated.Text style={{...Styles.logoText, opacity: opacity2}}>
-            {text[1]}
-          </Animated.Text>
-          <Animated.Text style={{...Styles.logoText, opacity: opacity3}}>
-            {text[2]}
-          </Animated.Text>
-          <Animated.Text style={{...Styles.logoText, opacity: opacity3}}>
-            {text[3]}
-          </Animated.Text>
+          {text.map((char, index) => (
+            <Animated.Text
+              key={index}
+              style={[
+                Styles.logoText,
+                useAnimatedStyle(() => ({
+                  opacity: translations[index].value,
+                  transform: [{ translateY: translations[index].value * 20 }],
+                })),
+              ]}
+            >
+              {char}
+            </Animated.Text>
+          ))}
         </View>
       </View>
     </SafeAreaView>
